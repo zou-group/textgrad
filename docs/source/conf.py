c633e08b657ff284249789d1d6ec03237a7cacde
vinid <chiccobia@gmail.com>
2024-06-11 18:17:23 -0400

    versioning fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2024, TextGrad authors'
 author = 'TextGrad authors'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.1.3'
 
 
 # -- General configuration ---------------------------------------------------
