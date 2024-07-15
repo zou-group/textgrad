3929e1a144ed9ce8effe02ef846e34705f431574
Vaibhav Nayel <vaibhavnayel@gmail.com>
2024-07-15 23:19:36 +0530

    made chatgroq subclass chatopenai

--- textgrad/engine/groq.py
+++ textgrad/engine/groq.py
@@ -16,10 +16,11 @@ from typing import List, Union
 
 from .base import EngineLM, CachedEngine
 from .engine_utils import get_image_type_from_bytes
+from .openai import ChatOpenAI
 
 
-class ChatGroq(EngineLM, CachedEngine):
-    DEFAULT_SYSTEM_PROMPT = "You are a helpful, creative, and smart assistant."
+class ChatGroq(ChatOpenAI):
+    DEFAULT_SYSTEM_PROMPT = "You are a helpful, creative, and smart assistant1."
 
     def __init__(
         self,
@@ -33,10 +34,10 @@ class ChatGroq(EngineLM, CachedEngine):
         """
         root = platformdirs.user_cache_dir("textgrad")
         cache_path = os.path.join(root, f"cache_groq_{model_string}.db")
-        super().__init__(cache_path=cache_path)
+        CachedEngine.__init__(self, cache_path=cache_path)
+        
         if os.getenv("GROQ_API_KEY") is None:
             raise ValueError("Please set the GROQ_API_KEY environment variable if you'd like to use Groq models.")
-        
         self.client = Groq(
             api_key=os.getenv("GROQ_API_KEY")
         )
@@ -44,85 +45,5 @@ class ChatGroq(EngineLM, CachedEngine):
         self.model_string = model_string
         self.system_prompt = system_prompt
         assert isinstance(self.system_prompt, str)
-
-    @retry(wait=wait_random_exponential(min=1, max=5), stop=stop_after_attempt(5))
-    def generate(self, content: Union[str, List[Union[str, bytes]]], system_prompt: str=None, **kwargs):
-        if isinstance(content, str):
-            return self._generate_from_single_prompt(content, system_prompt=system_prompt, **kwargs)
-        
-        elif isinstance(content, list):
-            has_multimodal_input = any(isinstance(item, bytes) for item in content)
-            if has_multimodal_input:
-                raise NotImplementedError("Multimodal generation is not supported for Groq models.")
-            
-            return self._generate_from_multiple_input(content, system_prompt=system_prompt, **kwargs)
-
-    def _generate_from_single_prompt(
-        self, prompt: str, system_prompt: str=None, temperature=0, max_tokens=2000, top_p=0.99
-    ):
-
-        sys_prompt_arg = system_prompt if system_prompt else self.system_prompt
-        cache_or_none = self._check_cache(sys_prompt_arg + prompt)
-        if cache_or_none is not None:
-            return cache_or_none
-
-        response = self.client.chat.completions.create(
-            model=self.model_string,
-            messages=[
-                {"role": "system", "content": sys_prompt_arg},
-                {"role": "user", "content": prompt},
-            ],
-            frequency_penalty=0,
-            presence_penalty=0,
-            stop=None,
-            temperature=temperature,
-            max_tokens=max_tokens,
-            top_p=top_p,
-        )
-
-        response = response.choices[0].message.content
-        self._save_cache(sys_prompt_arg + prompt, response)
-        return response
-
-    def __call__(self, prompt, **kwargs):
-        return self.generate(prompt, **kwargs)
-
-    def _format_content(self, content: List[Union[str, bytes]]) -> List[dict]:
-        """Helper function to format a list of strings and bytes into a list of dictionaries to pass as messages to the API.
-        """
-        formatted_content = []
-        for item in content:
-            if isinstance(item, str):
-                formatted_content.append({
-                    "type": "text",
-                    "text": item
-                })
-            else:
-                raise ValueError(f"Unsupported input type: {type(item)}")
-        return formatted_content
-
-    def _generate_from_multiple_input(
-        self, content: List[Union[str, bytes]], system_prompt=None, temperature=0, max_tokens=2000, top_p=0.99
-    ):
-        sys_prompt_arg = system_prompt if system_prompt else self.system_prompt
-        formatted_content = self._format_content(content)
-
-        cache_key = sys_prompt_arg + json.dumps(formatted_content)
-        cache_or_none = self._check_cache(cache_key)
-        if cache_or_none is not None:
-            return cache_or_none
-
-        response = self.client.chat.completions.create(
-            model=self.model_string,
-            messages=[
-                {"role": "system", "content": sys_prompt_arg},
-                {"role": "user", "content": formatted_content},
-            ],
-            temperature=temperature,
-            max_tokens=max_tokens,
-            top_p=top_p,
-        )
-
-        response_text = response.choices[0].message.content
-        self._save_cache(cache_key, response_text)
-        return response_text
+        self.is_multimodal = False
+        breakpoint()
