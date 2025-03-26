648cd60cd752cbc7bfbb8b286069076c6b8a632e
vinid <chiccobia@gmail.com>
2025-03-26 13:08:22 -0400

    never let me code again

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md") as readme_file:
 
 setup(
     name="textgrad",
-    version="0.1.7",
+    version="0.1.8",
     description="",
     python_requires=">=3.9",
     classifiers=[
