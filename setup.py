20e8bf77fc2cc7414a24e22271eb37b13266c338
vinid <chiccobia@gmail.com>
2025-03-26 13:01:19 -0400

    bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md") as readme_file:
 
 setup(
     name="textgrad",
-    version="0.1.6",
+    version="0.1.7",
     description="",
     python_requires=">=3.9",
     classifiers=[
